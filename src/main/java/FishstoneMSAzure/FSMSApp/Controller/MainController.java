e0878ae9fea1b3a35d84a84caf0841ac149343f2
Computer <leonard.r.wecke@gmail.com>
2021-04-02 16:57:12 +0200

    New Projectstructure

--- src/main/java/FishstoneMSAzure/FSMSApp/Controller/MainController.java
+++ src/main/java/FishstoneMSAzure/FSMSApp/Controller/MainController.java
@@ -1,4 +1,4 @@
-package FishstoneMSAzure.FSMSApp;
+package FishstoneMSAzure.FSMSApp.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,6 @@ public class MainController {
     @GetMapping("/")
     public String hello()
     {
-        return "Hello, Azure!";
+        return "HelloWorld";
     }
 }
